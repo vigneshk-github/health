a30120ec017cdad49b40e5984141ec365356be8f
vignesh <vigneshten5@gmail.com>
2024-07-21 10:06:55 +0530

    3

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,18 +1,22 @@
-import style from "./Hero.module.css"
+import style from "./Hero.module.css";
 // import img1 from '../../Image/img1.png'
+import video from "../../Image/vid.mp4";
 
-export default function Hero(){
-    return (
-      <div className={style.hero}>
-        <div className={style.left}>
-          <h1>Enovation in Every Pulse</h1>
-          <span>
-            Shaping the future of health with breakthrough innovations that
-            promote physical, mental, and spiritual wellness
-          </span>
-          <button>Explore </button>
+export default function Hero() {
+  return (
+    <div className={style.hero}>
+      <video className={style.video} src={video} autoPlay loop muted></video>
+      <div className={style.text}>
+        <h1>Inovation in Every Pulse</h1>
+        <p>
+          Shaping the future of health with breakthrough innovations that
+          promote physical, mental, and spiritual wellness
+        </p>
+        <div className={style.button}>
+          <button className={style.button2}>Explore</button>
+          <button className={style.button2}>Contact</button>
         </div>
-        <div className={style.right}>{/* <img src={img1} /> */}</div>
       </div>
-    );
+    </div>
+  );
 }
